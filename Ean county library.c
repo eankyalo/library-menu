d56297ba02aa1e956e467bd530cad90ace90e26e
eankyalo <93204055+eankyalo@users.noreply.github.com>
2021-10-27 21:22:20 +0300

    Add files via upload

---
+++ Ean county library.c
@@ -0,0 +1,23 @@
+/*  Project: County Library system
+    Author: Ean Kyalo
+    Date: 25/06/2021
+    Compiler: GCC
+    Licence: MIT
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCOUNTY LIBRARY\n");
+    printf("Welcome to the Library\n");
+    printf("What would you like to do in the Library?\n");
+    printf("1. New user? Enter your details to sign up.\n");
+    printf("2. Search for a book.\n");//search tool
+    printf("3. Borrow a book\n");//Borrowing a book
+    printf("4:Update user: \n");//updating the user profile
+    printf("5:Change password: \n");//changing password
+    printf("6:Log out => \n");//log out of the system optio
+}
+
